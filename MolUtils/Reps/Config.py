2ce2495f9fc4493419a86f229681e73ee95f82c5
Shehtab Zaman <szaman5@binghamton.edu>
2023-10-17 20:46:18 -0400

    Adding an example notebook

--- MolUtils/Reps/Config.py
+++ MolUtils/Reps/Config.py
@@ -2,7 +2,13 @@ from rdkit import Chem
 
 ChargeDict = {'H': 1, 'C': 6, 'N': 7, 'O': 8} 
 
-BondDict = [None, Chem.rdchem.BondType.SINGLE, Chem.rdchem.BondType.DOUBLE, Chem.rdchem.BondType.TRIPLE,
+BondDict = [None,
+            Chem.rdchem.BondType.SINGLE,
+            Chem.rdchem.BondType.DOUBLE,
+            Chem.rdchem.BondType.TRIPLE,
             Chem.rdchem.BondType.AROMATIC]
 
 OrganicDecoder = ['H', "C", 'N', "O"]
+
+def GetIndex(list, element):
+  return list.index(element)
--- MolUtils/Reps/SmilesToGraph.py
+++ MolUtils/Reps/SmilesToGraph.py
@@ -4,6 +4,8 @@ from Config import ChargeDict
 
 
 def SmilesToGraph(smiles_string: str,
+                  atom_embedding_func,
+                  bond_embedding_func,
                   removeHs=True,
                   dense=False):
   _mol = Chem.MolFromSmiles(smiles_string)
@@ -11,11 +13,44 @@ def SmilesToGraph(smiles_string: str,
   if _mol == None:
     raise ValueError('Inavlid smiles string')
   
+  data = {}
   _mol = _mol if removeHs else Chem.AddHs(_mol)
 
   num_atoms = _mol.GetNumAtoms()
+  data['num_atoms'] = num_atoms
+
   charges = np.array([ChargeDict[x] for x in _mol.GetAtoms()])
+  data['charge'] = charges
   
+  atom_mat = np.array([atom_embedding_func(x.GetSymbol()) for x in _mol.GetAtoms()])
+  data['atoms'] = atom_mat
+
+  if len(_mol.GetBonds()) > 1:
+    edge_mat = []
+    if dense:
+      adj_mat = np.zeros((num_atoms, num_atoms))
+      edge_mat = []
+    else:
+      # Return COO format connectivity
+      edge_list = []
+    
+    for bond in _mol.GetBonds():
+      i = bond.GetBeginAtomIdx()
+      j = bond.GetEndAtomIdx()
+      if dense:
+        adj_mat[i,j] = 1
+        edge_mat[i,j] = bond_embedding_func(bond)
+      else:
+        edge_list.append([i, j])
+        edge_mat.append(bond_embedding_func(bond))
+    
+    data['edge_type'] = edge_mat
+    if dense:
+      data['edge_mat'] = adj_mat
+    else:
+      data['edge_list'] = edge_list
+
+  return data
   
 
 
